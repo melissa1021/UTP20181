6a15154091a44fb14d4d9d3464c76135c8876c37
oz090 <oaarias@utp.edu.co>
2018-02-22 17:44:27 -0500

    Final Comentado

--- CS/ejemploDos/client.py
+++ CS/ejemploDos/client.py
@@ -1,59 +1,68 @@
 import zmq
 import sys
 import os
-import md5
 
+# Tamaño del trozo o porcion de bit que vamos a pasar por red,
 tamanoTrozo = (1024*1024)
 
 def main():
-    if len(sys.argv) < 2:
-        print("Error calling the program")
+    # Se verifica que existan por lo menos cuatro parametros IP Puerto MetodoOperacion NombreArchivo
+    if len(sys.argv) < 4:
+        print("Verifica que existan por lo menos cuatro parametros IP Puerto MetodoOperacion NombreArchivo")
         exit()
 
+    # Se obtienen los argumentos 
     serverIp = sys.argv[1]
     serverPort = sys.argv[2]
     operation = sys.argv[3]
     filename = sys.argv[4]
-    #parteDescargar = 0
 
+    # Conexion con el servidor de sockets
     context = zmq.Context()
     s = context.socket(zmq.REQ)
     s.connect("tcp://{}:{}".format(serverIp, serverPort))
 
+    # Se verifica cual fue el metodo u operacion que el cliente solicito para enviar esto al servidor
     if operation == "list":
-        print('entro a list')
+        # Opcion list envia al servidor la petición de listar todos los archivos de carpeta en que inicio y me los imprima en consola.
         s.send_json({"op": "list"})
         files = s.recv_json()
         print(files)
 
     elif operation == "download":
+        #Opcion de descargar un archivo especifico, le debemos enviar al servidor el nombre del archivo y me lo pone en la carpeta raiz que me entrego el cliente.
             s.send_json({"op":"download", "file":filename})
             file = s.recv()
             with open("down-"+ operation, "wb") as output:
                 output.write(file)
 
     elif operation == "descargapartes":
+        # Descargar partes primero le envia al servidor la peticion de conocer de cuantas partes se compone el archivo, 
+        # luego hacemos un for desde la parte 0 a la N solicitando cada una de sus partes y las vamos uniendo para obtener 
+        # el total del archivo. Se realiza de esta manera para no saturar el servidor y que este pueda ir entragando parte 
+        # por parte a cada cliente que solicita.
+
+        # Solicitud de cantidad de partes que se guarda en la variable cantidad
             s.send_json({"op":"partes", "file":filename})
             files = s.recv_json()
             cantidad = files["partes"]
+
+        # Guardamos el archivo por partes con la palabra  "Final" adelante.
             destino = "final" + filename
 
+        # Se hace un recorrido desde la parte 0 a la N, se hace la peticion de cada parte al servidor, 
+        # una vez este contesta nos retorna el binario con esa parte solicitada
+        # la vamos "pegando" al final del binario para al final obtener el total
+
             for x in range(0, cantidad):
+                # Solicitud de cada una de las partes
                 s.send_json({"op":"parteN", "file":filename, "numP":x})
+                # Binario de la parte solicitada
                 file = s.recv()
+                
+                # Abrimos el archivo final y le pegamos al final el binario recibido hasta obtener el total
                 with open(destino, "ab") as dest:
                     dest.write(file)
-
-    elif operation == "partes":
-            s.send_json({"op":"partes", "file":filename})
-            files = s.recv_json()
-            print(files)
-
-    elif operation == "parteN":
-            s.send_json({"op":"parteN", "file":filename, "numP":parteDescargar})
-            file = s.recv()
-            with open("{} part{}".format(filename, parteDescargar), "wb") as output:
-                output.write(file)
 
 
 if __name__ == '__main__':
--- CS/ejemploDos/server.py
+++ CS/ejemploDos/server.py
@@ -3,7 +3,6 @@ import math
 import zmq
 import sys
 import os
-import md5
 
 tamano_trozo = 1024*1024
 
@@ -17,14 +16,20 @@ def loadFiles(path):
     return files
 
 def partes(localPath):
-    #Retorna el tamano el megas
+    # Retorna el tamano del archivo buscado, para ello utiliza os.path.getSize eso me retorna el total de bits, 
+    # luego lo dividimos para obtener el total de megas y pasar por red una mega a la vez y no saturar el servidor
     b = os.path.getsize(localPath)
     return math.ceil(b/(1024*1024))
 
 def main():
-    if len(sys.argv) != 3:
-        print("Error calling the program")
+    # Verificamos si nos envian exactamente 3 argumentos
+    if len(sys.argv) < 2:
+        print("Por favor ingresa las variables 1 Puerto / 2 Carpeta de archivos")
         exit()
+
+    # Guardamos dichos argumentos en las variables, 
+    # port -> puerto 
+    # filesdir ->directorio de archivos 
 
     port = sys.argv[1]
     filesDir = sys.argv[2]
@@ -33,26 +38,49 @@ def main():
     s = context.socket(zmq.REP)
     s.bind("tcp://*:{}".format(port))
 
+    # Se cargan todo los archivos a la variable files, por meido del procedimiento files
     files = loadFiles(filesDir)
     print(files)
 
+    # inciamos un demonio o cliclo infinito para esperar que un socket se conecte y nos hable
     while True:
         print("Waiting for request")
+
+        # Recibimos la peticion en un socket y o recibimos en JSON
         msg = s.recv_json()
+
+        # De acuerdo al apeticion recibida filtramos y hacemos el proceso necesario.
         if msg["op"] == "list":
+            # Se listan el total de archivos y se retornan en un JSON con clave files.
             s.send_json({"files": list(files.keys())})
+
         elif msg["op"] == "download":
+
+            # Solicitud de un archivo especifico, recibe en el JSON con clave file, el nombre le archivo solicitado y se lo retorna completo al cliente.
             with open(sys.argv[2]+msg["file"],"rb") as input:
 	            data = input.read()
 	            s.send(data)
+
         elif msg["op"] == "partes":
+            # Retorna la cantidad de partes que contiene un archivo, cada parte es de tamaño (tamano_trozo) para eso utiliza la funcion definida arriba 'partes'
             data = partes(sys.argv[2]+msg["file"])
+
+            # Una vez obtenido la cantidad de partes se retornan por el mismo socket que nos hablaron
             s.send_json({"partes": data})
+            
         elif  msg["op"] == "parteN":
+
+            # Para no saturar el servidor y dar la impresion de concurrencia, se pediran los archivos por trozos, 
+            # para ello el cliente nos pedira el numero del trozo que quiere y se lo retornaremos como binario, es responsabilidad del cliente unir nuevamente los trozos
             with open(sys.argv[2]+msg["file"],"rb") as input:
+
+                # En caso del que este solicitando el primer trozo se hace un read desde 0 por defecto hasta tamano_trozo
                 if msg["numP"] == "0":
                     data = input.read(tamano_trozo)
                 else:
+                
+                # Cuando es un trozo diferente se parsea el numero a int se multiplica por el tamaño de cada trozo, 
+                # luego se busca con input.seek donde esta ubicado esa porcion de bits y se retorna al cliente.
                     posicion = int(msg["numP"])*tamano_trozo
                     input.seek(posicion)
                     data = input.read(tamano_trozo)
